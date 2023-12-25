63a01d9f8ec5526bde2c583b54ffbae7ad15bc39
Satyam <58844449+satyam7world@users.noreply.github.com>
2023-12-25 10:20:08 +0530

    Initial commit

---
+++ src/main/kotlin/com/github/satyam7world/springbootdockergithubaction/SpringBootDockerGithubActionApplication.kt
@@ -0,0 +1,11 @@
+package com.github.satyam7world.springbootdockergithubaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootDockerGithubActionApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootDockerGithubActionApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/satyam7world/springbootdockergithubaction/SpringBootDockerGithubActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.satyam7world.springbootdockergithubaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootDockerGithubActionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
